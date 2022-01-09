981ec5fe8a220c757c0ed9ad8487f70ac18c3b6e
Jake <Jake.Laoyu@gmail.com>
2022-01-09 14:41:01 +0800

    chore: remove copy config

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,11 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import copy from "rollup-plugin-copy";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   console.log(mode);
   return {
-    plugins: [
-      vue(),
-      copy({
-        targets: [
-          mode === "production"
-            ? {
-                src: resolve(__dirname, "./src/styles/fonts"),
-                dest: resolve(__dirname, "./dist/assets/"),
-              }
-            : {
-                src: resolve(__dirname, "./src/styles/fonts"),
-                dest: resolve(__dirname, "public/"),
-              },
-        ],
-        hook: "writeBundle", // notice here
-      }),
-    ],
+    plugins: [vue()],
     css: { preprocessorOptions: { scss: { charset: false } } },
   };
 });
